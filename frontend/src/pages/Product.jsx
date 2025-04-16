5aebcfa33c68e8267a71fa56979708b4fe077462
Sai Shyam <85333271+saishyam36@users.noreply.github.com>
2025-04-16 22:09:12 +0530

    changing alignment of the images

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,6 +2,7 @@
 import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 import { ShopContext } from "../context/ShopContext";
+import { currency } from "../utils/constant";
 
 const Product = () => {
 
@@ -30,22 +31,22 @@ const Product = () => {
         {/* Product Data */}
         <div className="flex gap-10 flex-row">
           {/* Product Images */}
-          <div className="flex-1 flex flex-row gap-3">
-            <div className="flex flex-col gap-y-3 overflow-x-auto">
+          <div className="flex-1 flex flex-col-reverse gap-3 justify-items-center border-2 border-inherit border-gray-500">
+            <div className="flex flex-row gap-x-3 justify-center overflow-auto">
               {
                 productData.image.map((item, index) => (
-                  <img src={item} key={index} className="w-[30%] cursor-pointer" alt='' />
+                  <img onClick={()=> setImage(item)} src={item} key={index} className="w-[20%] cursor-pointer" alt='' />
                 ))
               }
             </div>
-            <div className="w-full">
-              <img src={image} alt="" className="w-full h-auto"></img>
+            <div className="flex justify-center">
+              <img src={image} alt="" className="w-[80%] h-auto"></img>
             </div>
           </div>
           {/* Product Info */}
           <div className="flex-1">
-            <h1 className="font-medium text-2xl mt-2">{productData.name}</h1>
-
+            <h1 className="font-sans text-3xl mt-2">{productData.name}</h1>
+            <p className="mt-5 text-3xl font-medium">{currency}{productData.price}</p>
           </div>
 
         </div>
