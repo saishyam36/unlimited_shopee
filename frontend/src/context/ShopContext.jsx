f840255e13f869f92df893d114390fad04ed9294
Sai Shyam <85333271+saishyam36@users.noreply.github.com>
2025-04-19 21:49:12 +0530

    add to cart code added

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,15 +1,39 @@
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/prop-types */
-import { createContext } from "react";
+import { createContext, useEffect, useMemo, useState } from "react";
 import { products } from "../assets/assets";
 import { currency, deliveryFee } from "../utils/constant";
 
 export const ShopContext = createContext();
 
-const ShopContextProvider = (props) =>{
-    const value= {
-        products,currency,deliveryFee
+const ShopContextProvider = (props) => {
+    const [cartItems, setCartItems] = useState({});
+
+    const addToCart = (itemId, size) => {
+        let cartData = structuredClone(cartItems);
+
+        if (cartData[itemId]) {
+            if (cartData[itemId][size] > 0) {
+                cartData[itemId][size] += 1
+            } else {
+                cartData[itemId][size] = 1
+            }
+        } else {
+            cartData[itemId] = {}
+            cartData[itemId][size] = 1
+        }
+        setCartItems(cartData);
     }
+
+    useEffect(() => {
+        console.log(cartItems);
+    }, [cartItems])
+
+
+    const value = useMemo(() => ({
+        products, currency, deliveryFee, cartItems, addToCart
+    }), [products, currency, deliveryFee, addToCart, cartItems]);
+
     return (
         <ShopContext.Provider value={value}>
             {props.children}
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -3,17 +3,18 @@ import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 import { ShopContext } from "../context/ShopContext";
 import { currency } from "../utils/constant";
-import { Button } from "antd";
+import { Button, message } from "antd";
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import RelatedProducts from "../components/RelatedProducts";
 
 const Product = () => {
 
   const { productID } = useParams();
-  const { products } = useContext(ShopContext);
+  const { products, addToCart } = useContext(ShopContext);
   const [productData, setProductData] = useState('');
   const [image, setImage] = useState('')
   const [size, setSize] = useState('')
+  const [isErrorVisible, setIsErrorVisible] = useState(false);
 
   const fetchProductData = () => {
     products.map((item) => {
@@ -25,62 +26,82 @@ const Product = () => {
     })
   }
 
-  useEffect(() => {
-    fetchProductData();
-    setSize('')
-  }, [productID])
+  const handleAddToCart = () => {
+    console.log(size, 'ss')
+    if (size !== '') {
+      addToCart(productData._id, size)
+    } else if (!isErrorVisible) {
+        message.error({
+          content: 'Please select product size',
+          duration: 3,
+          top: 100,
+          className: 'productError',
+          onClose: () => {
+            setIsErrorVisible(false);
+          },
+        });
+        setIsErrorVisible(true)
+      }
+    }
 
-  return (
-    productData ? (
-      <div className="border-t-2 pt-10 py-40">
-        {/* Product Data */}
-        <div className="flex gap-10 flex-row">
-          {/* Product Images */}
-          <div className="flex-1 flex flex-col-reverse gap-3 justify-items-center border-2 border-inherit border-gray-500 w-[50%]">
-            <div className="flex flex-row gap-x-3 justify-center overflow-auto">
-              {
-                productData.image.map((item, index) => (
-                  <img onClick={() => setImage(item)} src={item} key={index} className="w-[20%] cursor-pointer" alt='' />
-                ))
-              }
-            </div>
-            <div className="flex justify-center">
-              <img src={image} alt="" className="w-[55%] h-auto"></img>
-            </div>
+useEffect(() => {
+  fetchProductData();
+  setSize('')
+}, [productID])
+
+return (
+  productData ? (
+    <div className="border-t-2 pt-10 py-40">
+      {/* Product Data */}
+      <div className="flex gap-10 flex-row">
+        {/* Product Images */}
+        <div className="flex-1 flex flex-col-reverse gap-3 justify-items-center border-2 border-inherit border-gray-500 w-[50%]">
+          <div className="flex flex-row gap-x-3 justify-center overflow-auto">
+            {
+              productData.image.map((item, index) => (
+                <img onClick={() => setImage(item)} src={item} key={index} className="w-[20%] cursor-pointer" alt='' />
+              ))
+            }
           </div>
-          {/* Product Info */}
-          <div className="flex-1">
-            <h1 className="font-semibold text-2xl mt-2">{productData.name}</h1>
-            {/* TODO : add star rating related code here */}
-            <p className="mt-5 text-3xl font-mono">{currency}{productData.price}</p>
-            <p className="mt-5 text-gray-500 md:w-4/5">{productData.description}</p>
-            <div className="flex flex-col gap-4 my-8">
-              <p>Select Size</p>
-              <div className="flex gap-7">
-                {productData.sizes.map((item, index) => (
-                  <Button onClick={() => setSize(item)} size="large" key={index} type="default" shape="circle" className={`${item === size ? 'bg-cyan-100' : 'bg-slate-100'}`}>
-                    {item}
-                  </Button>
-                ))}
-              </div>
-            </div>
-            <Button icon={<ShoppingCartOutlinedIcon fontSize="small" />} className="px-4 py-4 text-sm my-2" onClick={() => setSize(item)} size="large" color="default" variant="solid">Add To Cart</Button>
-            <hr className="mt-8 sm:w-4/5"></hr>
-            <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
-              <p>100% Original product.</p>
-              <p>Cash on delivery is available on this product.</p>
-              <p>Easy return and exchange policy within 7 days.</p>
-            </div>
+          <div className="flex justify-center">
+            <img src={image} alt="" className="w-[55%] h-auto"></img>
           </div>
         </div>
-          {/* {TODO : add review code here when added ratings code} */}
-          {/* {Display Related Products} */}
-          <div>
-            <RelatedProducts category={productData.category} subCategory={productData.subCategory} selectedProductID={productID} /> 
+        {/* Product Info */}
+        <div className="flex-1">
+          <h1 className="font-semibold text-2xl mt-2">{productData.name}</h1>
+          {/* TODO : add star rating related code here */}
+          <p className="mt-5 text-3xl font-mono">{currency}{productData.price}</p>
+          <p className="mt-5 text-gray-500 md:w-4/5">{productData.description}</p>
+          <div className="flex flex-col gap-4 my-8">
+            <p>Select Size</p>
+            <div className="flex gap-7">
+              {productData.sizes.map((item, index) => (
+                <Button onClick={() => setSize(item)} size="large" key={index} type="default" shape="circle" className={`${item === size ? 'bg-cyan-100' : 'bg-slate-100'}`}>
+                  {item}
+                </Button>
+              ))}
+            </div>
           </div>
-      </div>) :
-      (<div className="opacity-0"></div>)
-  )
+          <Button icon={<ShoppingCartOutlinedIcon fontSize="small" />} className="px-4 py-4 text-sm my-2" onClick={handleAddToCart} size="large" color="default" variant="solid">
+            Add To Cart
+          </Button>
+          <hr className="mt-8 sm:w-4/5"></hr>
+          <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
+            <p>100% Original product.</p>
+            <p>Cash on delivery is available on this product.</p>
+            <p>Easy return and exchange policy within 7 days.</p>
+          </div>
+        </div>
+      </div>
+      {/* {TODO : add review code here when added ratings code} */}
+      {/* {Display Related Products} */}
+      <div>
+        <RelatedProducts category={productData.category} subCategory={productData.subCategory} selectedProductID={productID} />
+      </div>
+    </div>) :
+    (<div className="opacity-0"></div>)
+)
 }
 
 export default Product
