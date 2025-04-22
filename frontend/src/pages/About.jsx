266c9b9eb63a6590b9437d0f1ba75f4933860633
Sai Shyam <85333271+saishyam36@users.noreply.github.com>
2025-04-22 21:30:46 +0530

    contact and about code added

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,26 @@
 // import React from 'react'
 
+import { assets } from "../assets/assets"
+import Title from "../components/Title"
+
 const About = () => {
   return (
-    <div>About</div>
+      <div className="pb-20">
+        <div className="my-2">
+          <div className="text-center py-2">
+            <Title text1={'About'} text2={'Us'}></Title>
+          </div>
+        </div>
+        <div className="my-10 flex flex-col md:flex-row gap-10">
+          <img className="w-full md:max-w-[550px]" src={assets.about_img} alt="" />
+          <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+            <p>Driven by a passion for innovation and a vision to reshape online shopping, Unlimited Shopee was created. Our core aspiration was to build a platform where everyone could easily discover, explore, and buy a wide range of products from the comfort of their own home</p>
+            <p>Since launch, we've focused on handpicking a diverse and high-quality selection of products to suit many tastes and needs. From the latest in fashion and beauty to cutting-edge electronics and essential home goods, our wide inventory comes proudly from trusted brands and suppliers.</p>
+            <b className="text-gray-800">Our Mission</b>
+            <p>At Unlimited Shopee, our core aim is to provide shoppers with unparalleled choice, ease, and assurance in their online purchases. We are committed to crafting a smooth and exceptional shopping journey that goes beyond standard expectations, encompassing every step from initial browsing and order placement to the final delivery and beyond.</p>
+          </div>
+        </div>
+      </div>
   )
 }
 
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,8 +1,29 @@
 // import React from 'react'
 
+import { assets } from "../assets/assets"
+import Title from "../components/Title"
+
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <div className="my-2">
+        <div className="text-center py-2">
+          <Title text1={'Contact'} text2={'Us'}></Title>
+        </div>
+      </div>
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-20 mb-28 mr-10">
+        <img className="w-full md:max-w-[580px]" src={assets.contact_img} alt="" />
+        <div className="flex flex-col justify-center items-start gap-6">
+          <p className="font-semibold text-2xl text-gray-600">Our Store</p>
+          <p className=" text-gray-500">
+            D.No. 36, Flat No. S-1, <br />
+            Srinagar Colony, Visakhapatnam, <br />
+            Andhra Pradesh 530016, India
+          </p>
+          <p className=" text-gray-500">Tel: +91-9258238986 <br /> Email: contact@unlimited.com</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
