6ffab6c0fbed1e49d7005d4375e54c56464c6e68
Sai Shyam <85333271+saishyam36@users.noreply.github.com>
2025-04-25 00:11:40 +0530

    added starting code

---
+++ backend/controllers/userController.js
@@ -0,0 +1,24 @@
+
+
+
+// User login route
+const loginUser = async (req, res) => {
+
+
+}
+
+
+
+// User registration route
+const registerUser = async (req, res) => {
+
+}
+
+
+// Admin login route
+const loginAdmin = async (req, res) => {
+
+}
+
+
+export { loginUser, registerUser }
