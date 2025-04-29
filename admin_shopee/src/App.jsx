7e21c543623d20544b18dbc01b3fc8207cc8c7cd
Sai Shyam <85333271+saishyam36@users.noreply.github.com>
2025-04-30 01:10:46 +0530

    adding svg icon and divider

--- admin_shopee/src/App.jsx
+++ admin_shopee/src/App.jsx
@@ -1,4 +1,4 @@
-import { Layout } from "antd"
+import { Divider, Layout } from "antd"
 import Navbar from "./components/Navbar.jsx"
 import Footer from "./components/Footer.jsx"
 
@@ -7,13 +7,11 @@ function App() {
   return (
     <Layout>
       <div className="px-4 sm:px-[4vw] md:px-[5vw] lg:px-[7vw]">
-
         <Navbar />
+        <Divider variant="solid" style={{ borderWidth: 1 }} />
 
         <Footer />
-
       </div>
-
     </Layout>
   )
 }
