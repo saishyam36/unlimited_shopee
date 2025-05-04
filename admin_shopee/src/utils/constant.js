c49730ec34a3ac9be5d6a4a7c9c25d83e1f829e8
Sai Shyam <85333271+saishyam36@users.noreply.github.com>
2025-05-05 00:27:31 +0530

    added .env variable for admin application

--- admin_shopee/src/utils/constant.js
+++ admin_shopee/src/utils/constant.js
@@ -6,7 +6,7 @@ export const copyRightText = 'Copyright 2025@ Unlimited shopee - All Right Reser
 export const shopDescription = 'Explore a vast and diverse world of products, ranging from the everyday essentials that simplify your life to the most unique and hard-to-find treasures. Our intuitive shopping app brings you a carefully curated selection, designed with effortless navigation and a completely seamless purchasing experience, all at your fingertips'
 export const relatedProductsLength = 4;
 export const maxProductImages = 4;
-export const backendApiUrl = 'http://localhost:4000/api';
+export const backendApiUrl = import.meta.env.VITE_Unlimited_BACKEND_URL;
 
 export const productCategoryItems = [
   {
